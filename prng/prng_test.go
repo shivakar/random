6f70f3ee45f14e4dc267b4687ab663e16a91da1c
Shivakar Vulli <svulli@shivakar.com>
2017-06-09 19:29:05 -0400

    Removed benchmark for rand.Uint64 to allow package compatibility with Go 1.7

--- prng/prng_test.go
+++ prng/prng_test.go
@@ -13,12 +13,14 @@ func Benchmark_BuiltInPRNG_Int63(b *testing.B) {
 		_ = rng.Int63()
 	}
 }
+
+/*
 func Benchmark_BuiltInPRNG_Uint64(b *testing.B) {
 	rng := rand.New(rand.NewSource(int64(time.Now().UnixNano())))
 	for i := 0; i < b.N; i++ {
 		_ = rng.Uint64()
 	}
-}
+}*/
 
 func Benchmark_BuiltInPRNG_Float64(b *testing.B) {
 	rng := rand.New(rand.NewSource(int64(time.Now().UnixNano())))
